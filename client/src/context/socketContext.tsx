801649fa2228f66145c89cd0a4e38fb913d5b4d3
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-15 23:43:39 +0600

    Add socketContext and Provider
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/context/socketContext.tsx
+++ client/src/context/socketContext.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+// socketContext.ts
+import React, { createContext, useContext, FC, useEffect, useState } from "react";
+import { Socket, io } from "socket.io-client";
 
-const SocketContext = React.createContext(null);
+interface SocketProviderProps {
+      children?: React.ReactElement
+      userId?: string
+}
+const SocketContext = createContext<Socket | undefined>(undefined);
 
-export default SocketContext;
+export const SocketProvider: FC<SocketProviderProps> = ({ children, userId }) => {
+      const [socket, setSocket] = useState<Socket | undefined>(undefined);
+
+      useEffect(() => {
+            const newSocket = io(import.meta.env.VITE_APP_API);
+            newSocket.emit("joinRoom", userId);
+            setSocket(newSocket);
+
+            return () => {
+                  // Отключение сокета при размонтировании компонента
+                  newSocket.disconnect();
+            };
+      }, [userId]);
+      return (
+            <SocketContext.Provider value={socket}>
+                  {children}
+            </SocketContext.Provider>
+      );
+};
+
+export const useSocket = () => useContext(SocketContext);
