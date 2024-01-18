b0484990acb4a6d34acad87117c9fd4ec69093e0
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-18 23:24:39 +0600

    Add socket event on new notification
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/context/socketContext.tsx
+++ client/src/context/socketContext.tsx
@@ -1,6 +1,8 @@
 // socketContext.ts
 import React, { createContext, useContext, FC, useEffect, useState } from "react";
 import { Socket, io } from "socket.io-client";
+import { useAppDispatch } from "../hooks/redux";
+import { addNotification } from "../slices/notification.slice";
 
 interface SocketProviderProps {
       children?: React.ReactElement
@@ -10,15 +12,21 @@ const SocketContext = createContext<Socket | undefined>(undefined);
 
 export const SocketProvider: FC<SocketProviderProps> = ({ children, userId }) => {
       const [socket, setSocket] = useState<Socket | undefined>(undefined);
-
+      const dispatch = useAppDispatch()
       useEffect(() => {
             const newSocket = io(import.meta.env.VITE_APP_API);
             newSocket.emit("joinRoom", userId);
             setSocket(newSocket);
 
+
+            newSocket.on("notification", (data) => {
+                  dispatch(addNotification(data));
+            });
+
             return () => {
                   // Отключение сокета при размонтировании компонента
                   newSocket.disconnect();
+                  newSocket.off("notification");
             };
       }, [userId]);
       return (
