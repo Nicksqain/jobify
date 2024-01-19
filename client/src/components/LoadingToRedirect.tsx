ff485c9acaac0e90ca9ba089817d33d60a34a522
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-19 21:53:50 +0600

    LoadingToRegirect replace to Skeleton
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/components/LoadingToRedirect.tsx
+++ client/src/components/LoadingToRedirect.tsx
@@ -1,3 +1,4 @@
+import { Stack, Skeleton, Button, Box, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 import React, { FC } from "react";
 // Components
 import { ThreeDots } from "react-loader-spinner";
@@ -8,7 +9,15 @@ const LoadingToRedirect: FC = () => {
 
   return (
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <ThreeDots color="#73f190" wrapperStyle={{ marginTop: "0px" }} />
+      {/* <ThreeDots color="#73f190" wrapperStyle={{ marginTop: "0px" }} /> */}
+      <Stack spacing={1} mt={50} >
+        <Skeleton height='75px' mb={50} isLoaded={false}>
+        </Skeleton>
+        <SkeletonCircle size='20' mb={30} />
+
+        <SkeletonText noOfLines={4} mb={10} spacing='4' skeletonHeight='6' />
+        <SkeletonText noOfLines={4} spacing='4' skeletonHeight='6' />
+      </Stack>
       {/* <ThreeDots color="#5f5cee" wrapperStyle={{ marginTop: "0px" }} /> */}
     </div>
   );
