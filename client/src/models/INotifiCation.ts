6f5fd0dc58151f8449b25524be9809ac7b3365a6
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-19 21:57:29 +0600

    Update INotification model
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/models/INotifiCation.ts
+++ client/src/models/INotifiCation.ts
@@ -1,7 +1,12 @@
 export interface INotification {
-  id: number;
-  message: string;
-  isRead: boolean;
-  userId: string;
-  createdAt: Date;
+  id?: number;
+  message?: string;
+  reason?: string;
+  source?: string;
+  type?: string;
+  object?: string;
+  subject?: string;
+  isRead?: boolean;
+  userId?: string;
+  createdAt?: Date;
 }
