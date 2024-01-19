3c0f691af07a4c3ddacac468e742147aa346fbad
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-19 21:56:41 +0600

    Add ModerationIcon icon component
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

---
+++ client/src/assets/icons/ModerationIcon.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+function ModerationIcon() {
+      return (
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256">
+                  <path fill="none" d="M0 0H256V256H0z"></path>
+                  <path
+                        fill="none"
+                        stroke="#000"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="24"
+                        d="M40 114.7V56a8 8 0 018-8h160a8 8 0 018 8v58.7c0 84-71.3 111.8-85.5 116.5a7.2 7.2 0 01-5 0C111.3 226.5 40 198.7 40 114.7z"
+                  ></path>
+                  <path
+                        fill="none"
+                        stroke="#000"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="24"
+                        d="M128 92L128 132"
+                  ></path>
+                  <circle cx="128" cy="172" r="16"></circle>
+            </svg>
+      );
+}
+
+export default ModerationIcon;
