5b28a2b9d449a39e2e8da381006092b75beea5da
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-21 23:25:03 +0600

    Update useUpdateOrderStatusMutation hook
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/mutations/orderMutations.ts
+++ client/src/mutations/orderMutations.ts
@@ -6,16 +6,22 @@ export const useUpdateOrderStatusMutation = () => {
   const queryClient = useQueryClient();
 
   return useMutation(
-    (data: { orderId: number; status: string; reason: string }) =>
+    (data: {
+      orderId: number;
+      status: string;
+      reason?: string | null;
+      commentType?: string | null;
+    }) =>
       updateOrderStatus(data.orderId, {
         status: data.status,
         reason: data.reason,
+        commentType: data.commentType,
       }),
     {
       onSuccess: () => {
         // После успешного выполнения мутации можно сбросить кэш запроса,
         // чтобы обновить данные в соответствии с изменениями
-        queryClient.invalidateQueries(["orders"]);
+        queryClient.invalidateQueries(["orders", "userOrders"]);
       },
     }
   );
