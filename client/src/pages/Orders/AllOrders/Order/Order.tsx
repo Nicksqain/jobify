6c62c29d3b839bcca6609b829f87227b989f601e
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-23 22:07:06 +0600

    Update Order component
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/pages/Orders/AllOrders/Order/Order.tsx
+++ client/src/pages/Orders/AllOrders/Order/Order.tsx
@@ -11,7 +11,10 @@ import Prompt from "../../../../components/ui/Prompt/Prompt";
 import { isAdmin, isModerator } from "../../../../helpers/role";
 import { useUpdateOrderStatusMutation } from "../../../../mutations/orderMutations";
 import toast from "react-hot-toast";
-import { AlertDialog, AlertDialogOverlay, AlertDialogContent, AlertDialogHeader, AlertDialogBody, AlertDialogFooter, Button, useDisclosure, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay } from "@chakra-ui/react";
+import { Button, useDisclosure, FormControl, FormLabel, Input, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, FormErrorMessage } from "@chakra-ui/react";
+import { useAppSelector } from "../../../../hooks/redux";
+import TaskResponses from "./TaskResponses/TaskResponses";
+import TaskDetails from "./TaskDetails/TaskDetails";
 
 
 interface OrderProps {
@@ -31,6 +34,7 @@ interface OrderProps {
 
 const Order: FC<OrderProps> = ({ id, title, role, budget, description, author, createdAt, servicePlace, isUserOrder = true }) => {
 
+  const { user } = useAppSelector(state => state.userSlice)
   //  Cancel order part
   const { isOpen, onOpen, onClose } = useDisclosure()
   const cancelRef = useRef<HTMLButtonElement>(null);
@@ -42,50 +46,6 @@ const Order: FC<OrderProps> = ({ id, title, role, budget, description, author, c
 
   const updateOrderStatusMutation = useUpdateOrderStatusMutation();
 
-  const handleShowPrompt = () => {
-    setIsPromptOpen(true);
-  };
-
-  const handleOrderCancelingPromptConfirm = async () => {
-
-    try {
-      // Предположим, что у вас есть orderId и другие данные для обновления
-      const orderId = id;
-      const status = 'rejected';
-      const reason = confirmValue;
-
-      // Вызовите мутацию
-      await updateOrderStatusMutation.mutateAsync({ orderId, status, reason });
-
-      // Закройте модальное окно или что у вас там
-      setIsPromptOpen(false);
-    } catch (error) {
-      toast.error('Ошибка при обновлении статуса заказа')
-      console.error('Ошибка при обновлении статуса заказа', error);
-    }
-    setIsPromptOpen(false);
-  };
-
-  const handlePromptCancel = () => {
-    // Обработка отмены
-    console.log('Prompt cancelled');
-    setIsPromptOpen(false);
-  };
-  // 
-  const handleShowConfirm = () => {
-    console.log("check")
-    setIsConfirmOpen(prevIsConfirmOpen => !prevIsConfirmOpen);
-  };
-
-  const handleConfirm = () => {
-    // Обработка подтверждения
-    setIsConfirmOpen(false);
-  };
-
-  const handleCancel = () => {
-    // Обработка отмены
-    setIsConfirmOpen(false);
-  };
 
   const servicePlaces: Record<string, string> = {
     myPlace: "У меня",
@@ -109,57 +69,184 @@ const Order: FC<OrderProps> = ({ id, title, role, budget, description, author, c
     }
   }
 
-  if (role === "freelancer") {
-    return (
-      <div className="order">
-        <div className="order-header">
-          <div className="top">
-            <div className="left">
-              <div className="order-title">{title}</div>
-              <div className="views">
-                <FontAwesomeIcon icon={faEye} /> <span>135</span>
-              </div>
-            </div>
-            <div className="right">
-              <div className="price-range">{parseBudget()} KZT</div>
-            </div>
-          </div>
-          <div className="bottom">
-            <div className="creation-date">Создано 03 октября 2022</div>
-          </div>
-        </div>
-        <div className="order-details">
-          <div className="top">
-            <div className="left">
-              <div className="order-service-place">
-                <p className="title">Место оказания услуги</p>
-                <span>Любое</span>
-              </div>
-            </div>
-            <div className="right">
-              <div className="orderer">
-                <p className="title">Заказчик</p>
-                <span className="person-name">Ренат Аримов</span>
-              </div>
-            </div>
-          </div>
-          <div className="bottom">
-            <div className="order-description">
-              <p className="title">Описание услуги</p>
-              <div className="description">
-                Ищем Тильда-гения Человека, который умеет делать красивые сайты
-                на тильде, есть около 4-6 сайтов-лендингов, которые нужно
-                сделать, присылайте свои резюме в виде уже готовых
-                сайтов/дизайнов, цены на лендинг со свой точки зрения тоже
-                пишите сразу. Всем спасибо, ждём талант
-              </div>
-            </div>
-            <button className="write-orderer">Написать заказчику</button>
-          </div>
-        </div>
-      </div>
-    );
-  }
+  const [reason, setReason] = useState<string | null>(null);
+  const [isReasonValid, setIsReasonValid] = useState<boolean | undefined>(false);
+
+  const handleReasonChange = (event) => {
+    const inputValue = event.target.value;
+    setReason(inputValue);
+
+    setIsReasonValid(inputValue !== '');
+  };
+
+  const handleCloseByModer = async () => {
+    if (isReasonValid) {
+      try {
+        const orderId = id;
+        const status = 'rejected';
+        const commentType = "Moder";
+        const response = await updateOrderStatusMutation.mutateAsync({ orderId, status, reason, commentType });
+        if (response) {
+          onClose();
+          setReason('')
+          setIsReasonValid(false)
+        }
+      } catch (error) {
+        toast.error('Ошибка при обновлении статуса задачи')
+      }
+
+    }
+  };
+
+  const handleCloseByUser = async () => {
+    try {
+      const orderId = id;
+      const status = 'rejected';
+      const response = await updateOrderStatusMutation.mutateAsync({ orderId, status });
+      if (response) {
+        onClose();
+      }
+    } catch (error) {
+      toast.error('Ошибка при обновлении статуса задачи')
+    }
+  };
+
+  // if (role === "freelancer") {
+  //   return (
+  //     <div className="order">
+  //       <div className="order-header">
+  //         <div className="top">
+  //           <div className="left">
+  //             <div className="order-title">{title}</div>
+  //             <div className="views">
+  //               <FontAwesomeIcon icon={faEye} /> <span>135</span>
+  //             </div>
+  //           </div>
+  //           <div className="right">
+  //             <div className="price-range">{parseBudget()} KZT</div>
+  //           </div>
+  //         </div>
+  //         <div className="bottom">
+  //           <div className="creation-date">Создано 03 октября 2022</div>
+  //         </div>
+  //       </div>
+  //       <div className="order-details">
+  //         <Tabs>
+  //           <Tab title="Order details">
+  //             <div className="top">
+  //               <div className="left">
+  //                 <div className="order-service-place">
+  //                   <p className="title">Место оказания услуги</p>
+  //                   <span>{getServicePlaceString(servicePlace)}</span>
+  //                 </div>
+  //               </div>
+  //               <div className="right">
+  //                 <div className="orderer">
+  //                   <p className="title">Заказчик</p>
+  //                   <span className="person-name">{author}</span>
+  //                 </div>
+  //               </div>
+  //             </div>
+  //             <div className="bottom">
+  //               <div className="order-description">
+  //                 <p className="title">Описание услуги</p>
+  //                 <div className="description">
+  //                   {description}
+  //                 </div>
+  //               </div>
+
+  //             </div>
+  //           </Tab>
+
+  //           {
+  //             isUserOrder ? (<Tab title="Responses">
+  //               <div className="responses">
+  //                 <div className="response">
+  //                   <div className="header">
+  //                     <div className="person">
+  //                       <div className="avatar">
+  //                         <img src="images/users/avatars/avatar.jpg" alt="" />
+  //                       </div>
+  //                       <div className="data">
+  //                         <div className="fullname">
+  //                           <span>Алексей Кариков</span>
+  //                           <FontAwesomeIcon icon={faCircleCheck} />
+  //                         </div>
+  //                         <div className="date">17.12.2022</div>
+  //                       </div>
+  //                     </div>
+  //                     <button className="my-btn discuss-btn">
+  //                       Обсудить условия
+  //                     </button>
+  //                   </div>
+  //                   <div className="footer">
+  //                     <div className="price">Стоимость: 35 000 KZT</div>
+  //                     <div className="description-text">
+  //                       Добрый день, заинтересовало ваше предложение. Готова
+  //                       сделать для вас крутой лендинг. Имею большой опыт в
+  //                       разработке сайтов. Мои работы вы можете посмотреть по
+  //                       ссылке (портфолио): https://youdo.com/u3849629
+  //                     </div>
+  //                   </div>
+  //                 </div>
+  //                 {/* <div className="response">2</div> */}
+  //               </div>
+  //             </Tab>) : (<></>)
+  //           }
+
+  //         </Tabs>
+  //         <div className="dflex justify-content-end">
+  //           {/* {isConfirmOpen ? <></> : (<button className="my-btn my-btn-danger" onClick={handleShowConfirm}>Cancel order</button>)}
+  //           <Confirm isOpen={isConfirmOpen} message="Are you sure?" onConfirm={handleConfirm} onCancel={handleCancel} /> */}
+  //           {
+  //             (!isUserOrder) && <>
+  //               <Button colorScheme='green' onClick={onOpen}>
+  //                 Взять в работу
+  //               </Button>
+  //               <Modal
+  //                 initialFocusRef={initialFocusRef}
+  //                 finalFocusRef={initialFocusRef}
+  //                 isOpen={isOpen}
+  //                 onClose={onClose}
+  //                 isCentered
+  //               >
+  //                 <ModalOverlay />
+  //                 <ModalContent>
+  //                   <ModalHeader>Отмена задачи</ModalHeader>
+  //                   <ModalCloseButton />
+  //                   <ModalBody pb={6}>
+  //                     {
+  //                       isAdmin(user) || isModerator(user) ?
+  //                         <FormControl isRequired isInvalid={setIsReasonValid}>
+  //                           <FormLabel>Причина</FormLabel>
+  //                           <Input ref={initialFocusRef} placeholder='Наличие сквернословия' onChange={handleReasonChange} />
+  //                           {
+  //                             !isReasonValid && <FormErrorMessage>Обязательно введите причину.</FormErrorMessage>
+  //                           }
+
+  //                         </FormControl>
+  //                         :
+  //                         <>Вы уверены, что хотите отклонить свою задачу?</>
+  //                     }
+  //                   </ModalBody>
+
+  //                   <ModalFooter>
+  //                     <Button onClick={onClose}>
+  //                       Закрыть
+  //                     </Button>
+  //                     <Button colorScheme='red' onClick={isAdmin(user) || isModerator(user) ? handleCloseByModer : handleCloseByUser} ml={3}>
+  //                       Отменить задачу
+  //                     </Button>
+  //                   </ModalFooter>
+  //                 </ModalContent>
+  //               </Modal>
+  //             </>
+  //           }
+  //         </div>
+  //       </div>
+  //     </div>
+  //   );
+  // }
   if (role === "orderer") {
     return (
       <div className="order">
@@ -182,64 +269,12 @@ const Order: FC<OrderProps> = ({ id, title, role, budget, description, author, c
         <div className="order-details">
           <Tabs>
             <Tab title="Order details">
-              <div className="top">
-                <div className="left">
-                  <div className="order-service-place">
-                    <p className="title">Место оказания услуги</p>
-                    <span>{getServicePlaceString(servicePlace)}</span>
-                  </div>
-                </div>
-                <div className="right">
-                  <div className="orderer">
-                    <p className="title">Заказчик</p>
-                    <span className="person-name">{author}</span>
-                  </div>
-                </div>
-              </div>
-              <div className="bottom">
-                <div className="order-description">
-                  <p className="title">Описание услуги</p>
-                  <div className="description">
-                    {description}
-                  </div>
-                </div>
-
-              </div>
+              <TaskDetails servicePlace={servicePlace} author={author} description={description} />
             </Tab>
 
             {
               isUserOrder ? (<Tab title="Responses">
-                <div className="responses">
-                  <div className="response">
-                    <div className="header">
-                      <div className="person">
-                        <div className="avatar">
-                          <img src="images/users/avatars/avatar.jpg" alt="" />
-                        </div>
-                        <div className="data">
-                          <div className="fullname">
-                            <span>Алексей Кариков</span>
-                            <FontAwesomeIcon icon={faCircleCheck} />
-                          </div>
-                          <div className="date">17.12.2022</div>
-                        </div>
-                      </div>
-                      <button className="my-btn discuss-btn">
-                        Обсудить условия
-                      </button>
-                    </div>
-                    <div className="footer">
-                      <div className="price">Стоимость: 35 000 KZT</div>
-                      <div className="description-text">
-                        Добрый день, заинтересовало ваше предложение. Готова
-                        сделать для вас крутой лендинг. Имею большой опыт в
-                        разработке сайтов. Мои работы вы можете посмотреть по
-                        ссылке (портфолио): https://youdo.com/u3849629
-                      </div>
-                    </div>
-                  </div>
-                  {/* <div className="response">2</div> */}
-                </div>
+                <TaskResponses />
               </Tab>) : (<></>)
             }
 
@@ -248,7 +283,7 @@ const Order: FC<OrderProps> = ({ id, title, role, budget, description, author, c
             {/* {isConfirmOpen ? <></> : (<button className="my-btn my-btn-danger" onClick={handleShowConfirm}>Cancel order</button>)}
             <Confirm isOpen={isConfirmOpen} message="Are you sure?" onConfirm={handleConfirm} onCancel={handleCancel} /> */}
             {
-              (isUserOrder || isAdmin() || isModerator()) && <>
+              (isUserOrder || isAdmin(user) || isModerator(user)) && <>
                 <Button colorScheme='red' onClick={onOpen}>
                   Отменить
                 </Button>
@@ -264,17 +299,26 @@ const Order: FC<OrderProps> = ({ id, title, role, budget, description, author, c
                     <ModalHeader>Отмена задачи</ModalHeader>
                     <ModalCloseButton />
                     <ModalBody pb={6}>
-                      <FormControl>
-                        <FormLabel>Причина</FormLabel>
-                        <Input ref={initialFocusRef} placeholder='Наличие сквернословия' />
-                      </FormControl>
+                      {
+                        isAdmin(user) || isModerator(user) ?
+                          <FormControl isRequired isInvalid={setIsReasonValid}>
+                            <FormLabel>Причина</FormLabel>
+                            <Input ref={initialFocusRef} placeholder='Наличие сквернословия' onChange={handleReasonChange} />
+                            {
+                              !isReasonValid && <FormErrorMessage>Обязательно введите причину.</FormErrorMessage>
+                            }
+
+                          </FormControl>
+                          :
+                          <>Вы уверены, что хотите отклонить свою задачу?</>
+                      }
                     </ModalBody>
 
                     <ModalFooter>
                       <Button onClick={onClose}>
                         Закрыть
                       </Button>
-                      <Button colorScheme='red' onClick={onClose} ml={3}>
+                      <Button colorScheme='red' onClick={isAdmin(user) || isModerator(user) ? handleCloseByModer : handleCloseByUser} ml={3}>
                         Отменить задачу
                       </Button>
                     </ModalFooter>
