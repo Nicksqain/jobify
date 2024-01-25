3f2b42858e9c05f0310e55cd8b13469e29dc02f7
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-01-25 19:09:18 +0600

    Update role helper
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/helpers/role.ts
+++ client/src/helpers/role.ts
@@ -7,3 +7,11 @@ export const isAdmin = (user: IUser | null | undefined) => {
 export const isModerator = (user: IUser | null | undefined) => {
   return user?.role === "moderator";
 };
+
+export const isFreelancer = (user: IUser | null | undefined) => {
+  return user?.status === "freelancer";
+};
+
+export const isOrderer = (user: IUser | null | undefined) => {
+  return user?.status === "orderer";
+};
