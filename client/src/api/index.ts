5c0f1f4b4606c07e8cd2da92bd935bdbd9dd09cd
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-01 23:00:33 +0600

    Add default Authorization header to axios config
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,8 +1,11 @@
 import axios from "axios";
+import { getFromLocalStorage } from "../helpers/auth";
 
 const api = axios.create({
   baseURL: `${import.meta.env.VITE_APP_API}`,
 });
+api.defaults.headers.common["Authorization"] =
+  getFromLocalStorage("auth").token;
 api.defaults.headers.common["ngrok-skip-browser-warning"] = "true";
 // const api = axios.create({
 //   baseURL: `${import.meta.env.VITE_APP_API}`,
