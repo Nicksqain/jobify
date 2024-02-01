28b48c01782d105dbef4413c6ee8ebb9a5b70172
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-01 23:01:13 +0600

    Update fetchData
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/api/fetchData.ts
+++ client/src/api/fetchData.ts
@@ -1,10 +1,21 @@
 import api from "./";
 
-export const fetchAllOrders = () =>
-  api.get("/orders").then((response) => response.data);
+export const fetchAllOrders = (queryCfg?: { [key: string]: any }) => {
+  const params = new URLSearchParams(queryCfg).toString();
+  const queryParams = params ? `?${params}` : "";
+  return api.get(`/orders/${queryParams}`).then((response) => response.data);
+};
 
-export const fetchAllUserOrders = (userId: string) =>
-  api.get(`/orders/${userId}`).then((response) => response.data);
+export const fetchAllUserOrders = (
+  userId: string,
+  queryCfg?: { [key: string]: any }
+) => {
+  const params = new URLSearchParams(queryCfg).toString();
+  const queryParams = params ? `?${params}` : "";
+  return api
+    .get(`/orders/${userId}${queryParams}`)
+    .then((response) => response.data);
+};
 
 export const fetchAllUserNotifications = (userId: string) =>
   api.get(`/notifications/${userId}`).then((response) => response.data);
