c3fa5db5f97558caa31b16c16a82f06a9001cbee
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-08 21:41:38 +0600

    Create CreateProject component
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

---
+++ client/src/pages/Projects/CreateProject/CreateProject.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface CreateProjectProps {
+
+}
+
+const CreateProject: FC<CreateProjectProps> = ({ }) => {
+      return (
+            <div>
+                  CreateProject
+            </div>
+      )
+}
+
+export default CreateProject;
