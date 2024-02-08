991a60521c940c627f9d3f525d802d9833b4c73d
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-08 21:41:24 +0600

    Create Projects component
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

---
+++ client/src/pages/Projects/Projects.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from 'react'
+
+interface ProjectsProps {
+
+}
+
+const Projects: FC<ProjectsProps> = ({ }) => {
+      return (
+            <div>
+                  Projects
+            </div>
+      )
+}
+
+export default Projects;
