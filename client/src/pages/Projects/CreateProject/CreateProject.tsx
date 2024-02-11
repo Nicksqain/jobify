baaa6c29daf32e7b267eb300014f52b5337f8a56
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-11 23:43:24 +0600

    Update CreateProject component
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/pages/Projects/CreateProject/CreateProject.tsx
+++ client/src/pages/Projects/CreateProject/CreateProject.tsx
@@ -1,15 +1,63 @@
-import React, { FC } from 'react'
+import { Checkbox, Stack } from '@chakra-ui/react'
+import React, { FC, useEffect } from 'react'
 
 interface CreateProjectProps {
 
 }
 
 const CreateProject: FC<CreateProjectProps> = ({ }) => {
+      const [checkedItems, setCheckedItems] = React.useState([
+            { id: 1, isChecked: false },
+            { id: 2, isChecked: false },
+            { id: 3, isChecked: false },
+      ]);
+
+      useEffect(() => {
+            console.debug(checkedItems)
+      }, [checkedItems])
+      const handleCheckboxChange = (id: number) => {
+            setCheckedItems((prevCheckedItems) =>
+                  prevCheckedItems.map((item) =>
+                        item.id === id ? { ...item, isChecked: !item.isChecked } : item
+                  )
+            );
+      };
+
       return (
-            <div>
-                  CreateProject
-            </div>
-      )
+            <>
+                  {checkedItems.length > 0 && (
+                        <>
+                              <Checkbox
+                                    isChecked={checkedItems.every((item) => item.isChecked)}
+                                    isIndeterminate={
+                                          checkedItems.some((item) => item.isChecked) &&
+                                          !checkedItems.every((item) => item.isChecked)
+                                    }
+                                    onChange={(e) => {
+                                          const newCheckedState = checkedItems.map((item) => ({
+                                                ...item,
+                                                isChecked: e.target.checked,
+                                          }));
+                                          setCheckedItems(newCheckedState);
+                                    }}
+                              >
+                                    Parent Checkbox
+                              </Checkbox>
+                              <Stack pl={6} mt={1} spacing={1}>
+                                    {checkedItems.map((item) => (
+                                          <Checkbox
+                                                key={item.id}
+                                                isChecked={item.isChecked}
+                                                onChange={() => handleCheckboxChange(item.id)}
+                                          >
+                                                Child Checkbox {item.id}
+                                          </Checkbox>
+                                    ))}
+                              </Stack>
+                        </>
+                  )}
+            </>
+      );
 }
 
 export default CreateProject;
