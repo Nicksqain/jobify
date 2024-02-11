8eebffcc228c70a4ce8d24c6140b357398083da0
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-11 23:43:13 +0600

    Update Projects component
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/pages/Projects/Projects.tsx
+++ client/src/pages/Projects/Projects.tsx
@@ -1,4 +1,8 @@
+import { Box, Button, Card, Checkbox, HStack, Heading, Stack, Text, VStack } from '@chakra-ui/react';
 import React, { FC } from 'react'
+import ProjectCard from './Project/ProjectCard';
+import Project from './Project/Project';
+import { Link } from 'react-router-dom';
 
 interface ProjectsProps {
 
@@ -6,9 +10,17 @@ interface ProjectsProps {
 
 const Projects: FC<ProjectsProps> = ({ }) => {
       return (
-            <div>
-                  Projects
-            </div>
+            <Box mb={50}>
+                  <Stack mt={55} spacing={10}>
+
+                        <HStack mb={10} justifyContent="space-between"><Heading size="md">Проекты</Heading>
+                              <Link to={"/projects/create"}><Button>Создать проект</Button></Link>
+                        </HStack>
+                        <Project />
+                        <Project />
+
+                  </Stack>
+            </Box>
       )
 }
 
