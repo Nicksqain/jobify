1599dec1e5bfaf4368730a3ec0132440f44844f1
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-15 12:50:54 +0600

    Обновления компонента проектов
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/pages/Projects/Projects.tsx
+++ client/src/pages/Projects/Projects.tsx
@@ -1,27 +1,52 @@
-import { Box, Button, Card, Checkbox, HStack, Heading, Stack, Text, VStack } from '@chakra-ui/react';
-import React, { FC } from 'react'
-import ProjectCard from './Project/ProjectCard';
+import { Box, Button, HStack, Heading, Stack, Text } from '@chakra-ui/react';
+import { FC } from 'react'
 import Project from './Project/Project';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { useAppSelector } from '../../hooks/redux';
+import { fetchProjects } from '../../api/projectApi';
+import { IProject } from '../../models/IProject';
 
 interface ProjectsProps {
 
 }
 
 const Projects: FC<ProjectsProps> = ({ }) => {
+      const { user } = useAppSelector(state => state.userSlice)
+
+      const {
+            data,
+            isLoading,
+            isError,
+            isSuccess,
+      } = useQuery<IProject[]>({
+            queryKey: ["allProjects"],
+            queryFn: () => fetchProjects(),
+      });
+
+
       return (
             <Box mb={50}>
                   <Stack mt={55} spacing={10}>
-
                         <HStack mb={10} justifyContent="space-between"><Heading size="md">Проекты</Heading>
                               <Link to={"/projects/create"}><Button>Создать проект</Button></Link>
                         </HStack>
-                        <Project />
-                        <Project />
+                        {
+                              (isError || (data && data.length <= 0)) &&
+                              <Text>Не удалось получить проекты!</Text>
+
+                        }
+                        {
+                              data?.map(el =>
+                                    <Project name={el.name} shortDescription={el.shortDescription} orders={el.orders} />
+                              )
+                        }
 
                   </Stack>
             </Box>
       )
 }
+
+
 
 export default Projects;
