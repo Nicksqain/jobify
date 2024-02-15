3b5e767a908565a64f9d3f2f91a5eb986969ae43
Alexei Karikov <karikov.alist.ru@gmail.com>
2024-02-16 02:25:24 +0600

    создание апишки откликов
    Signed-off-by: Alexei Karikov <karikov.alist.ru@gmail.com>

--- client/src/api/responsesApi.ts
+++ client/src/api/responsesApi.ts
@@ -1,7 +1,7 @@
 import api from ".";
 
 export const fetchOutgoingResponses = async (userId: string) =>
-  api.get(`/projects/outgoing/${userId}`).then((response) => response.data);
+  api.get(`/responses/outgoing/${userId}`).then((response) => response.data);
 
 export const fetchIncomingResponses = async (userId: string) =>
   api.get(`/responses/incoming/${userId}`).then((response) => response.data);
